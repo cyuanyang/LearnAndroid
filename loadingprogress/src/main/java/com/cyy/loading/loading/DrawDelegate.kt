127bce119ea93fe95f64f945f6e14b5d66c3e63e
yuanyang <yuanyang.chen@xstudy.com.cn>
2017-10-11 15:25:33 +0800

    放微博点击头像加载动画

---
+++ loadingprogress/src/main/java/com/cyy/loading/loading/DrawDelegate.kt
@@ -0,0 +1,8 @@
+package com.cyy.loading.loading
+
+/**
+ * Created by study on 17/10/11.
+ *
+ * 画图的代理
+ */
+
--- loadingprogress/src/main/java/com/cyy/loading/loading/LoadingProgress.kt
+++ loadingprogress/src/main/java/com/cyy/loading/loading/LoadingProgress.kt
@@ -93,13 +93,26 @@ private class LoadingProgressDrawable : Drawable() {
     private val paint:Paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val spotPaint = Paint(Paint.ANTI_ALIAS_FLAG)
     private var startAngle = -90f
-    private var sweepAngle:Float = 360f
+    private var sweepAngle:Float = 0f
     private val rectF:RectF = RectF()
+    private var spotOffset = 10
+
+//    /**
+//     * 控制画风格  偶数为弧度增加 奇数为弧度减少
+//     */
+//    private var drawStyle:Int = 0
+//        set(value) {
+//            if (value==100){
+//                field = 0
+//            }
+//            field = value
+//        }
 
     var beginAngle = -90f //开始的位置
     var orginSweepAngle = 360f //最开始的角度
 
     var rotateAngle:Float = 0f
+
 
     var strokeWidth = 0f
         set(value) {
@@ -114,7 +127,6 @@ private class LoadingProgressDrawable : Drawable() {
         paint.strokeCap = Paint.Cap.ROUND
 
         spotPaint.style = Paint.Style.STROKE
-//        spotPaint.color = Color.RED
         spotPaint.strokeCap = Paint.Cap.ROUND
         spotPaint.strokeWidth = strokeWidth
     }
@@ -124,23 +136,25 @@ private class LoadingProgressDrawable : Drawable() {
     override fun draw(canvas: Canvas) {
         val count = canvas.save()
         val rotate = rotateAngle
-        canvas.rotate(rotate, rectF.centerX() , rectF.centerY())
+//        canvas.rotate(rotate, rectF.centerX() , rectF.centerY())
 
         val angle = drawProgressBar(canvas)
         drawSpot(canvas , angle)
         canvas.restoreToCount(count)
     }
 
+    /**
+     * 画进度条
+     */
     private fun drawProgressBar(canvas: Canvas):Angle{
         sweepAngle = 720f * level/10000 + orginSweepAngle
-
         var actualSweepAngle = sweepAngle
         if (actualSweepAngle>360){
             startAngle = actualSweepAngle - 360 + beginAngle
             actualSweepAngle = 360f - (startAngle - beginAngle )
         }
         canvas.drawArc(rectF , startAngle , actualSweepAngle , false , paint)
-
+        //todo 创建对象不要在onDraw
         return Angle(startAngle , actualSweepAngle)
     }
 
@@ -150,23 +164,48 @@ private class LoadingProgressDrawable : Drawable() {
     private fun drawSpot(canvas: Canvas , angle: Angle){
         val count = canvas.save()
         canvas.translate(rectF.centerX() , rectF.centerY())
-        canvas.drawPoints(genSpotPoints(angle.start , 0f ) , spotPaint)
+        drawPoints(canvas , angle.start , 0f)
         canvas.restoreToCount(count)
     }
 
-    fun genSpotPoints(start:Float , sweep: Float):FloatArray{
-        return arrayListOf<Float>().apply {
-            var  a = beginAngle
-            while (a < start ){
-                a += 10
-                log("a === $a")
-                val radus = rectF.centerX()-strokeWidth
-                val startX = Math.cos(Math.toRadians(a.toDouble()))*radus
-                val startY = Math.sin(Math.toRadians(a.toDouble()))*radus
-                add(startX.toFloat() )
-                add(startY.toFloat())
+    /**
+     * 计算画出了多少个点点
+     *
+     * 根据点点的位置设置制定的大小
+     */
+    private fun drawPoints(canvas: Canvas, start:Float ,sweep: Float){
+        var  a = beginAngle
+
+        //计算能话多少个点点
+        var count = ((start - a)/spotOffset).toInt()
+        count = when{
+            count>360/spotOffset -> 360/spotOffset
+            else -> count
+        }
+
+        var i = 0
+        while (a < start){
+            a += spotOffset
+
+            if (a > start){
+                break
             }
-        }.toFloatArray()
+            //计算点点的X Y
+            val radus = rectF.centerX()-strokeWidth
+            val radians = Math.toRadians(a.toDouble())
+            val startX = Math.cos(radians)*radus
+            val startY = Math.sin(radians)*radus
+            //点点的宽度递增
+            spotPaint.strokeWidth = strokeWidth * i.toFloat()/count
+            canvas.drawPoint(startX.toFloat() , startY.toFloat() , spotPaint)
+
+            if (a - 360 > beginAngle){
+                break
+            }
+            i ++
+        }
+
+        log("connt == $count  i == $i")
     }
 
     override fun setAlpha(alpha: Int) {
