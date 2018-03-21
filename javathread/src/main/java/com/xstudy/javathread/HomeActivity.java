4804a7d6aa409427660d20c331b5325b70f050aa
cyuanyang <shawn199101@163.com>
2018-03-21 22:21:51 +0800

    同步

---
+++ javathread/src/main/java/com/xstudy/javathread/HomeActivity.java
@@ -0,0 +1,40 @@
+package com.xstudy.javathread;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by chenyuanyang on 2018/3/21.
+ */
+
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    protected Button lockBtn;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.activity_home);
+        initView();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.lockBtn) {
+            goActivity(MainActivity.class);
+        }
+    }
+
+    private void initView() {
+        lockBtn = findViewById(R.id.lockBtn);
+        lockBtn.setOnClickListener(HomeActivity.this);
+    }
+
+    private void goActivity(Class clazz){
+        startActivity(new Intent(this ,clazz));
+    }
+}
---
+++ javathread/src/main/java/com/xstudy/javathread/SimpleActivity.java
@@ -0,0 +1,8 @@
+package com.xstudy.javathread;
+
+/**
+ * Created by chenyuanyang on 2018/3/21.
+ */
+
+public class SimpleActivity {
+}
