f1873bbb336ff2d601ebfafd2000741cc7310e55
yuanyang <chenyuanyang@100tal.com>
2018-04-19 10:58:18 +0800

    android thead

---
+++ javathread/src/main/java/com/xstudy/javathread/BlockingQueueActivity.java
@@ -0,0 +1,16 @@
+package com.xstudy.javathread;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.concurrent.BlockingQueue;
+
+public class BlockingQueueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blocking_queue);
+
+    }
+}
--- javathread/src/main/java/com/xstudy/javathread/SyncActivity.java
+++ javathread/src/main/java/com/xstudy/javathread/SyncActivity.java
@@ -7,20 +7,28 @@ import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Created by chenyuanyang on 2018/3/21.
- *
- *
  */
 
 public class SyncActivity extends SimpleActivity implements View.OnClickListener {
+
+    protected Button countDownLatchBtn;
+    protected Button cyclicBarrierBtn;
+    private Semaphore semaphore = new Semaphore(0, true);
+    private List<Runnable> tasks = Collections.synchronizedList(new ArrayList<Runnable>());
+
+    private Executor executor = Executors.newFixedThreadPool(10);
 
     protected Button semaphoreBtn;
 
@@ -40,22 +48,25 @@ public class SyncActivity extends SimpleActivity implements View.OnClickListener
         super.onClick(view);
         if (view.getId() == R.id.semaphoreBtn) {
             semaphoreTest();
+        } else if (view.getId() == R.id.countDownLatchBtn) {
+            countDownLatchTest();
+        } else if (view.getId() == R.id.cyclicBarrierBtn) {
+            cyclicBarrierTest();
         }
     }
 
     private void initView() {
         semaphoreBtn = findViewById(R.id.semaphoreBtn);
         semaphoreBtn.setOnClickListener(SyncActivity.this);
+        countDownLatchBtn = (Button) findViewById(R.id.countDownLatchBtn);
+        countDownLatchBtn.setOnClickListener(SyncActivity.this);
+        cyclicBarrierBtn = (Button) findViewById(R.id.cyclicBarrierBtn);
+        cyclicBarrierBtn.setOnClickListener(SyncActivity.this);
     }
-    private Semaphore semaphore = new Semaphore(0,true);
-    private List<Runnable> tasks = Collections.synchronizedList(new ArrayList<Runnable>());
 
-    private Executor executor = Executors.newFixedThreadPool(10);
 
-    private void semaphoreTest(){
-
-        for (int i = 0; i < 30; i++) {
-
+    private void semaphoreTest() {
+        for (int i = 0; i < 10; i++) {
             final int finalI = i;
             tasks.add(new Runnable() {
                 @Override
@@ -65,18 +76,16 @@ public class SyncActivity extends SimpleActivity implements View.OnClickListener
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    outputMsgSafely("我是任务"+ finalI);
+                    outputMsgSafely("我是任务" + finalI);
                     semaphore.release();
                 }
             });
         }
-
         executor.execute(new Runnable() {
             @Override
             public void run() {
-
                 try {
-                    while (!tasks.isEmpty()){
+                    while (!tasks.isEmpty()) {
                         executor.execute(tasks.remove(0));
                         semaphore.acquire();
                     }
@@ -88,6 +97,75 @@ public class SyncActivity extends SimpleActivity implements View.OnClickListener
     }
 
 
-    private CountDownLatch countDownLatch = new CountDownLatch(1);
+    private CountDownLatch countDownLatch = new CountDownLatch(2);
+
+    private void countDownLatchTest() {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                outputMsgSafely("count task 1 begin");
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                outputMsgSafely("count task 1 end");
+                countDownLatch.countDown();
+                outputMsgSafely("剩下几个="+countDownLatch.getCount());
+            }
+        });
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                outputMsgSafely("count task 2 begin");
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                outputMsgSafely("count task 2 end");
+                countDownLatch.countDown();
+                outputMsgSafely("剩下几个="+countDownLatch.getCount());
+            }
+        });
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    outputMsgSafely("主线任务 等待任务1,2执行");
+                    countDownLatch.await();
+                    outputMsgSafely("任务1,2执行完毕");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private CyclicBarrier cyclicBarrier = new CyclicBarrier(5);
+    private void cyclicBarrierTest() {
+        for (int i = 0; i < 5; i++) {
+            final int finalI = i;
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(finalI * 1000);
+                        outputMsgSafely("玩家" + finalI + "等待游戏进入");
+                        outputMsgSafely("cyclicBarrier.getParties=" + cyclicBarrier.getParties());
+                        cyclicBarrier.await();
+                        outputMsgSafely("玩家" + finalI + "进入游戏");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        outputMsgSafely("InterruptedException 玩家" + finalI );
+                    } catch (BrokenBarrierException e) {
+                        e.printStackTrace();
+                        outputMsgSafely("BrokenBarrierException 玩家" + finalI);
+                    }
+                }
+            });
+        }
+    }
+
 
 }
